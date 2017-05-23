67005cb78ee72c4c6518d613bcfdcc321364ca72
mattcheang <cheangmatt@gmail.com>
2017-05-23 15:05:32 -0700

    create page layouts

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,19 @@
+class HomeController < ApplicationController
+  def welcome
+  end
+
+  def staff
+  end
+
+  def insurance
+  end
+
+  def payment
+  end
+
+  def contact
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "home/welcome"
+  get "/staff" => 'home#staff', as: :staff
+  get "/insurance" => 'home#insurance', as: :insurance
+  get "/payment" => 'home#payment', as: :payment
+  get "/contact" => 'home#contact', as: :contact
+  get "/about" => 'home#about', as: :about
+
+  resources :contacts, only: [:new, :create]
+  root 'home#welcome'
 end
